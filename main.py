b6059e4ad95d1028e90db20c260643357f425236
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2024-07-24 07:45:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,19 +22,25 @@ st.set_page_config(page_title="ConcreteXAI",
 # }
 # """
 # st.markdown(hide_github_icon, unsafe_allow_html=True)
+hide_github_icon = """
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+"""
+
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
+# st.markdown(
+#     """
+#     <style>
+#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+#     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+#     .viewerBadge_text__1JaDK {
+#         display: none;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 #######################################################################################
 # Sidebar interface and its elements
