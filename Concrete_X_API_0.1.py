e6dec4dc599f0df5b79df2dc3d74ba4f607d7021
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2024-07-24 07:43:42 -0600

    Update Concrete_X_API_0.1.py

--- Concrete_X_API_0.1.py
+++ Concrete_X_API_0.1.py
@@ -1,19 +1,6 @@
 # Charging the neccessary libraries
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Define all the pages
 p1 = st.Page("main.py", title="Home", icon="🏠", default= True)
 p2 = st.Page("Acerca.py", title="About", icon="ℹ️") # 👀
