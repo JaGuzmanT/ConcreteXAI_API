9c77c21f8d6fd268cb36f0322a7f974664593544
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2025-01-17 17:44:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ st.markdown(hide_github_icon, unsafe_allow_html=True)
 col1_ti, col2_ti, col3_ti = st.columns(3, vertical_alignment="center", gap="small")
 with col2_ti:
 	st.title(":green[ConcreteXAI API] 📈", anchor=False)
-	st.image("Images/logo.webp", width=300)
+	st.image("Images/logo.webp", width=350, use_column_width=True) 
 st.write(":fire: _This application is an interactive webapp for predicting the concrete performance using Non-destructive tests_ :fire:")
 st.divider()
 #######################################################################################
