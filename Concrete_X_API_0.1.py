602c8ff7a22f96475e7fc4a600d9ed91f2f9b046
José Alberto Guzmán Torres <64563486+JaGuzmanT@users.noreply.github.com>
2025-01-17 18:32:12 -0600

    Update Concrete_X_API_0.1.py

--- Concrete_X_API_0.1.py
+++ Concrete_X_API_0.1.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Define all the pages
 p1 = st.Page("main.py", title="Home", icon="🏠", default= True)
-p2 = st.Page("Acerca.py", title="About", icon="ℹ️") # 👀
+p2 = st.Page("About.py", title="About", icon="ℹ️") # 👀
 
 #Install Multipage
 pg = st.navigation([p1, p2])
